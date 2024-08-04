e3924f31e6e2a1e628781135e280d6306a2742a6
Luan Guedes <lcrisley98@gmail.com>
2024-08-04 15:10:49 -0300

    Implemention Insert Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,12 +27,19 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n================== TEST 2: Seller Find All ==================");
+		System.out.println("\n================== TEST 3: Seller Find All ==================");
 		list = sellerDao.findAll();
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-	}
+		
+		System.out.println("\n================== TEST 4: Seller Insert ==================");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		}
 }
+
+
